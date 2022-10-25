1238f1170b412657dfe68cf81da70029e1d110b1
lovegroa <66517776+lovegroa@users.noreply.github.com>
2022-10-25 21:52:20 +0100

    adding ga

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
 import '../styles/styles.css';
 import 'aos/dist/aos.css';
+import Script from 'next/script';
 
 const App = ({Component, pageProps}) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-FBZ7N3BWY6"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-FBZ7N3BWY6', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
+      <Component {...pageProps} />;
+    </>
+  );
 };
 
 export default App;
